f9704e5d854701aa93389bc34a8be650250ccb67
Kazunari-h <kazunario456@gmail.com>
2021-08-04 20:54:42 +0900

    Initial Commit

---
+++ vote/voteApp.swift
@@ -0,0 +1,17 @@
+//
+//  voteApp.swift
+//  vote
+//
+//  Created by Kazunari Hirosawa on 2021/08/04.
+//
+
+import SwiftUI
+
+@main
+struct voteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
